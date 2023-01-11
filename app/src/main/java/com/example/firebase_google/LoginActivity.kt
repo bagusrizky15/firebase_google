fa23b8023972f64c04be0b05e76fab857eedf778
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2023-01-12 01:32:36 +0700

    firebase google

---
+++ app/src/main/java/com/example/firebase_google/LoginActivity.kt
@@ -0,0 +1,32 @@
+package com.example.firebase_google
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.firebase_google.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_login)
+
+        val Req_Code:Int=123
+        var firebaseAuth= FirebaseAuth.getInstance()
+
+        // Configure Google Sign In inside onCreate mentod
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+// getting the value of gso inside the GoogleSigninClient
+        mGoogleSignInClient= GoogleSignIn.getClient(this,gso)
+// initialize the firebaseAuth variable
+        firebaseAuth= FirebaseAuth.getInstance()
+    }
+}
